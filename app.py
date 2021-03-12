68aafb2fe598c02c93d3f6549b7c000dc6060eb1
Gamattowicz <p.romanczuk31@gmail.com>
2021-03-12 22:28:47 +0100

    add session and config file

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+from datetime import timedelta
+from config import Config
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 
 @app.route('/')
@@ -8,18 +13,32 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/<name>')
-def welcome(name):
-    return render_template('welcome.html', name=f'Hey {name}')
-
-
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        user = request.form['nm']
-        return redirect(url_for('welcome', name=user))
+        session.permanent = True
+        name = request.form['nm']
+        session['name'] = name
+        return redirect(url_for('welcome'))
     else:
+        if 'name' in session:
+            return redirect(url_for('welcome'))
         return render_template('login.html')
+
+
+@app.route('/welcome')
+def welcome():
+    if 'name' in session:
+        name = session['name']
+        return render_template('welcome.html', name=name)
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('name', None)
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
